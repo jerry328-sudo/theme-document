0c84e006c8dcd0a7bff1ca49f451657eb65fdf8c
jerry328-sudo <2289307498@qq.com>
2025-08-15 09:43:19 +0800

    优化代码高亮样式，新增PowerShell语法支持

--- common/prism/prism.js
+++ common/prism/prism.js
@@ -1,5 +1,5 @@
 /* PrismJS 1.29.0
-https://prismjs.com/download.html#themes=prism&languages=markup+css+clike+javascript+bash+c+csharp+cpp+css-extras+docker+git+go+http+icon+ini+java+javadoclike+jq+js-extras+json+json5+markdown+markup-templating+nginx+php+phpdoc+php-extras+plsql+python+sql+typescript+visual-basic&plugins=line-highlight+line-numbers+highlight-keywords+remove-initial-line-feed+inline-color+toolbar+copy-to-clipboard */
+https://prismjs.com/download.html#themes=prism&languages=markup+css+clike+javascript+bash+c+csharp+cpp+css-extras+docker+git+go+http+icon+ini+java+javadoclike+jq+js-extras+json+json5+markdown+markup-templating+nginx+php+phpdoc+php-extras+plsql+powershell+python+sql+typescript+visual-basic&plugins=line-highlight+line-numbers+highlight-keywords+remove-initial-line-feed+inline-color+toolbar+copy-to-clipboard */
 var _self = "undefined" != typeof window ? window : "undefined" != typeof WorkerGlobalScope && self instanceof WorkerGlobalScope ? self : {},
     Prism = function (e) {
         var n = /(?:^|\s)lang(?:uage)?-([\w-]+)(?=\s|$)/i, t = 0, r = {}, a = {
@@ -2211,3 +2211,52 @@ Prism.languages["visual-basic"] = {
         }
     })) : console.warn("Copy to Clipboard plugin loaded before Toolbar plugin."))
 }();
+
+// PowerShell syntax highlighting
+!function(e){
+    var i = e.languages.powershell = {
+        comment: [
+            {
+                pattern: /(^|[^`])<#[\s\S]*?#>/,
+                lookbehind: !0
+            },
+            {
+                pattern: /(^|[^`])#.*/,
+                lookbehind: !0
+            }
+        ],
+        string: [
+            {
+                pattern: /"(?:`[\s\S]|[^`"])*"/,
+                greedy: !0,
+                inside: null
+            },
+            {
+                pattern: /'(?:[^']|'')*'/,
+                greedy: !0
+            }
+        ],
+        namespace: /\[[a-z](?:\[(?:\[[^\]]*\]|[^\[\]])*\]|[^\[\]])*\]/i,
+        boolean: /\$(?:false|true)\b/i,
+        variable: /\$\w+\b/,
+        function: [
+            /\b(?:Add|Approve|Assert|Backup|Block|Checkpoint|Clear|Close|Compare|Complete|Compress|Confirm|Connect|Convert|ConvertFrom|ConvertTo|Copy|Debug|Deny|Disable|Disconnect|Dismount|Edit|Enable|Enter|Exit|Expand|Export|Find|ForEach|Format|Get|Grant|Group|Hide|Import|Initialize|Install|Invoke|Join|Limit|Lock|Measure|Merge|Move|New|Open|Optimize|Out|Ping|Pop|Protect|Publish|Push|Read|Receive|Redo|Register|Remove|Rename|Repair|Request|Reset|Resize|Resolve|Restart|Restore|Resume|Revoke|Save|Search|Select|Send|Set|Show|Skip|Sort|Split|Start|Step|Stop|Submit|Suspend|Switch|Sync|Tee|Test|Trace|Unblock|Undo|Uninstall|Unlock|Unprotect|Unpublish|Unregister|Update|Use|Wait|Watch|Where|Write)-[a-z]+\b/i,
+            /\b(?:ac|cat|chdir|clc|cli|clp|clv|compare|copy|cp|cpi|cpp|cvpa|dbp|del|diff|dir|ebp|echo|epal|epcsv|epsn|erase|fc|fl|ft|fw|gal|gbp|gc|gci|gcs|gdr|gi|gl|gm|gp|gps|group|gsv|gu|gv|gwmi|iex|ii|ipal|ipcsv|ipsn|irm|iwmi|iwr|kill|lp|ls|man|md|mkdir|move|mp|mv|nal|ndr|ni|nmo|npssc|nsn|nv|ogv|oh|popd|ps|pushd|pwd|r|rbp|rcjb|rcsn|rd|rdr|ren|ri|rjb|rm|rmdir|rmo|rni|rnp|rp|rsn|rsnp|rujb|rv|rvpa|rwmi|sajb|sal|saps|sasv|sbp|sc|select|set|shcm|si|sl|sleep|sls|sort|sp|spjb|spps|spsv|start|sujb|sv|swmi|tee|trcm|type|where|wjb|write)\b/i
+        ],
+        keyword: /\b(?:Begin|Break|Catch|Class|Continue|Data|Define|Do|DynamicParam|Else|ElseIf|End|Exit|Filter|Finally|For|ForEach|From|Function|If|InlineScript|Param|Process|Return|Switch|Throw|Trap|Try|Until|Using|Var|While|Workflow)\b/i,
+        operator: {
+            pattern: /(^|\W)(?:!|-(?:b?(?:and|or|xor|not)|as|(?:does|i)?(?:eq|ne|[gl][te])|(?:i|c)?(?:s?like|match|contains|in)|replace|join|is(?:not)?|exact|casesensitive))\b/i,
+            lookbehind: !0
+        },
+        punctuation: /[|{}[\];(),.]/
+    };
+    i.string[0].inside = {
+        function: {
+            pattern: /(^|[^`])\$\((?:\$\([^\r\n()]*\)|(?!\$\()[^\r\n)])*\)/,
+            lookbehind: !0,
+            inside: i
+        },
+        boolean: i.boolean,
+        variable: i.variable
+    }
+}(Prism);
